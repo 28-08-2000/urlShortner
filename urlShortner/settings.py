d3520f2faaf846b01de468e5e8e6d9cfebc9443b
28-08-2000 <dharamsinghmeena2000@gmail.com>
2022-03-01 17:03:48 +0530

    changed the database to default db.sqlite3

--- urlShortner/settings.py
+++ urlShortner/settings.py
@@ -82,7 +82,11 @@ WSGI_APPLICATION = 'urlShortner.wsgi.application'
 
 # changed the database from sqlite to dj database url
 DATABASES = {
-    'default': dj_database_url.config()
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+    # 'default': dj_database_url.config()
 }
 
 
